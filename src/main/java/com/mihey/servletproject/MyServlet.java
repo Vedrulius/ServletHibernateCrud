7f67121237e141f9f7c07847492b27418fad512c
Mikhail Dolgov <otetc82@yandex.ru>
2020-12-19 12:00:35 +0300

    adding util package

--- src/main/java/com/mihey/servletproject/MyServlet.java
+++ src/main/java/com/mihey/servletproject/MyServlet.java
@@ -1,28 +1,38 @@
 package com.mihey.servletproject;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = {"/MyServlet"})
+//@WebServlet(urlPatterns = {"/MyServlet"})
 public class MyServlet extends HttpServlet {
 
     @Override
-    public void init() throws ServletException {
-
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter pw=resp.getWriter();
+
+        HttpSession session=req.getSession();
+
+        Integer count = (Integer) session.getAttribute("count");
+
+        PrintWriter pw = resp.getWriter();
         pw.println("<html>");
         pw.println("<h1> Hello people! </h1>");
         pw.println("</html>");
 
+        RequestDispatcher rd = req.getRequestDispatcher("/index.jsp");
+        rd.forward(req,resp);
     }
 
 //    @Override
